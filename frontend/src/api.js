74b190189cc9bd54840bea4b44e4c10b342769e1
PranavKdagadakai <pkdagadakai@gmail.com>
2025-07-15 11:49:44 +0530

    Some feature update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "./constants";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:8000/",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
